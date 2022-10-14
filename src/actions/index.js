78c09f370b0bf3f02fbab002e8a1efcdafd28582
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-14 12:35:25 +0500

    Add SHORTENED SYNTAX WITH REDUX THUNK

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,28 @@
 import jsonPlhldr from "../api/jsonPlhldr";
 
-export const fetchPosts =  () => {
-  
-  return async function (dispatch, getState) {
-
-    const response = await jsonPlhldr.get('/posts');
-    
+//TODO: totally equivalent to origin
+export const fetchPosts =  () => async dispatch => {
+  const response = await jsonPlhldr.get('/posts');
     dispatch({ type : 'FETCH_POSTS', payload: response})
-  }
-};
+}
 
+
+//? WE ARE DEFINING A FUNCTION (fetchPosts) 
+//? THAT IS GOING TO RETURN A FUNCTION (dispatch)
 
 // return {
 //   type: 'FETCH_POSTS',
 //   payload: response
+// };
+
+//* origin
+
+// export const fetchPosts =  () => {
+  
+//   return async function (dispatch, getState) {
+
+//     const response = await jsonPlhldr.get('/posts');
+    
+//     dispatch({ type : 'FETCH_POSTS', payload: response})
+//   }
 // };
