17e574f64974e16f29e62d3332189e4de8702951
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-14 08:48:39 +0500

    Add fetch bad approach

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,11 @@
-export const fetchPosts = () => {
+import jsonPlhldr from "../api/jsonPlhldr";
+
+export const fetchPosts = async () => {
+  // BAD approach!!!
+  const response = await jsonPlhldr.get('/posts');
+
   return {
-    type: 'FETCH_POSTS'
+    type: 'FETCH_POSTS',
+    payload: response
   };
 };
