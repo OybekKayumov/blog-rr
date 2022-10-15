7a9b77a20c115380339a29f118c97bda91c317e6
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-15 12:45:40 +0500

    Add action fetchPostsAndUsers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,22 +1,35 @@
 import _ from 'lodash';
 import jsonPlhldr from "../api/jsonPlhldr";
 
+export const fetchPostsAndUsers = async () => {
+  
+}
+
 //TODO: totally equivalent to origin
 export const fetchPosts = () => async dispatch => {
   const response = await jsonPlhldr.get('/posts');
   dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
-
-// private function
-const _fetchUser = _.memoize( async (id, dispatch) => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlhldr.get(`/users/${id}`);
   dispatch({ type: "FETCH_USER", payload: response.data })
-});
+};
 
 
-//* lodash memoize --> no changes
+
+//* lodash memoize --> WORKS
+// export const fetchUser = id => dispatch => _fetchUser(id, dispatch);
+
+// // private function _fetchUser
+// const _fetchUser = _.memoize( async (id, dispatch) => {
+//   const response = await jsonPlhldr.get(`/users/${id}`);
+//   dispatch({ type: "FETCH_USER", payload: response.data })
+// });
+
+
+
+//* lodash memoize --> no changes, doesn't work
 // export const fetchUser = function(id) { 
 //   return _.memoize(async function(dispatch) {
 //     const response = await jsonPlhldr.get(`/users/${id}`);
