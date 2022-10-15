47857e5afb71fe2e78a063f67f86ecbd98ed2905
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-10-15 08:07:44 +0500

    Add usersReducer

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -3,6 +3,11 @@ import { connect } from "react-redux";
 import { fetchUser } from "../actions";
 
 class UserHeader extends Component {
+
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
   render() {
     return(
       <div>
